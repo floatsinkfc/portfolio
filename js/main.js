0721f9d319dbb11d6fc15652ca630ce2337cf5a5
floatsinkfc <floatsinkfc@gmail.com>
2023-07-22 23:02:40 -0500

    Version 1.1

--- js/main.js
+++ js/main.js
@@ -1,24 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const header = document.querySelector("header");
 
-  let lastScrollPos = window.scrollY;
-
-  function handleScroll() {
-    const currentScrollPos = window.scrollY;
-
-    if (currentScrollPos > lastScrollPos) {
-      // Scrolling down, hide the header
-      header.classList.add("hidden");
-    } else {
-      // Scrolling up, show the header
-      header.classList.remove("hidden");
-    }
-
-    lastScrollPos = currentScrollPos;
-  }
-
-  window.addEventListener("scroll", handleScroll);
-});
 
 
 const inputs = document.querySelectorAll('.contact-input');
